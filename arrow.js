1db6a232bd09d20fc0cfdcfd3964ad41ad724106
RushikeshChamle <rushikeshchamle23@gmail.com>
2023-07-27 00:55:16 +0530

    created branch with the arrow

---
+++ arrow.js
@@ -0,0 +1,15 @@
+// let beta = "";
+
+// beta = function () {
+//   return "Hello Jarvis to the New World";
+// };
+
+// document.getElementById("demo").innerHTML = beta();
+
+let gamma = "";
+
+gamma = () => {
+  return "Hello Arrow key Functions to the JS programming language";
+};
+
+document.getElementById("demo").innerHTML = gamma();
